e200c2b895fbad4c4bdea7e0c490f3cec6b4cd11
Anik Mohanta <mohonto05@gmail.com>
2026-03-05 13:38:40 +0600

    added selectCategory() function for change btn stutas and get stutas id to API

--- script.js
+++ script.js
@@ -18,10 +18,31 @@ async function loadCategories() {
         const btn = document.createElement('button')
          btn.className = "btn btn-primary bg-[#15803D] w-full"
          btn.textContent = category.category_name;
+         btn.onclick = () => selectCategory(category.id, btn);
 
          categoriesContainer.appendChild(btn)
     })
+}
 
+async function selectCategory(categoryId, btn) {
+    console.log(categoryId, btn)
+    
+    const allButtons = document.querySelectorAll(
+    "#categoriesContainer button, #allTreesBtn",); 
+    allButtons.forEach(btn => {
+        btn.classList.remove(' bg-[#15803D]')
+        btn.classList.add('btn-outline')
+    })
+
+    btn.classList.add('bg-[#15803D]')
+    btn.classList.remove('btn-outline')
+
+
+    const res = await fetch(
+    `https://openapi.programming-hero.com/api/category/${categoryId}`);
+    const data = await res.json();
+    console.log(data);
+    // displayTrees(data.plants);
 
 }
 
